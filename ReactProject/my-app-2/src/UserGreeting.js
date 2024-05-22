80dd406a95b4974ecae45b8fb509e05de96feb5b
nitika <nitikaaer17@gmail.com>
2024-05-22 14:04:23 +0530

    React Project Updated

---
+++ ReactProject/my-app-2/src/UserGreeting.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class UserGreeting extends Component {
+  render() {
+    return (
+      <div>
+        Welcome Nitika
+      </div>
+    )
+  }
+}
+
+export default UserGreeting
